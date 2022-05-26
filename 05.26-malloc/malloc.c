73a019ffd8ba6ba575426cee1e8e3c8c0b6c99a8
Christopher Siu <christopher@siu.sh>
2022-05-26 13:24:34 -0700

    Added dynamic allocation example.

--- 05.26-malloc/malloc.c
+++ 05.26-malloc/malloc.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int *mkarr(int n) {
-    int arr[n];
+    int *arr;
+
+    /* To dynamically allocate space for "n" integers on the heap: */
+    arr = (int *)malloc(sizeof(int) * n);
 
     return arr;
 }
@@ -9,11 +13,21 @@ int *mkarr(int n) {
 int main(void) {
     int *arr = mkarr(4);
 
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 4;
+    arr[3] = 8;
+
     printf("arr (%p): %p\n", (void *)(&arr), (void *)arr);
     printf(" |-- %p:  %d\n", (void *)(&arr[0]), (arr[0]));
     printf(" |-- %p:  %d\n", (void *)(&arr[1]), (arr[1]));
     printf(" |-- %p:  %d\n", (void *)(&arr[2]), (arr[2]));
     printf(" +-- %p:  %d\n", (void *)(&arr[3]), (arr[3]));
+
+    /* To deallocate space on the heap:
+     * NOTE: The compiler won't deallocate heap memory for us; we dynamically
+     *       allocated it; we are responsible for deallocating it. */
+    free(arr);
 
     return 0;
 }
