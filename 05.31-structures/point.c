f8d1e7418376325db3857355ae17858827d4e1f6
Christopher Siu <christopher@siu.sh>
2022-05-31 13:15:12 -0700

    Added structure examples.

--- 05.31-structures/point.c
+++ 05.31-structures/point.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include "point.h"
+
+/* To define a function that takes and returns a structure: */
+Point swap(Point pt) {
+    int tmp = pt.x;
+    pt.x = pt.y;
+    pt.y = tmp;
+
+    return pt;
+}
+
+/* To define a function that takes a pointer to a structure: */
+void increment(Point *ptptr) {
+    /* NOTE: The dot operator has precedence over the unary star; either
+     *       parenthesize the star or use an arrow instead. */
+    ptptr->x++;
+    ptptr->y++;
+}
 
 int main(void) {
-    int x = 0;
-    int y = 0;
+    /* To declare and initialize a variable that is a structure: */
+    Point pt = {1, 2};
+
+    /* To access a member within a structure: */
+    pt.x = 3;
+
+    /* To call a function that takes and returns a structure: */
+    pt = swap(pt);
+
+    /* To call a function that takes a pointer to a structure: */
+    increment(&pt);
 
     printf("pt (%p):\n", (void *)&pt);
     printf(" |-- x (%p): %d\n", (void *)&pt.x, pt.x);
---
+++ 05.31-structures/point.h
@@ -0,0 +1,8 @@
+/* To introduce a new name for another type: */
+typedef struct point Point;
+
+/* To define a structure composed of two integers: */
+struct point {
+    int x;
+    int y;
+};
